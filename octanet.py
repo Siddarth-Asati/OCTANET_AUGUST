9abafb76b9c6ca12c57f654d43038cea3e24d5b3
Siddarth-Asati <shidharthasati@gmail.com>
2024-08-10 17:26:26 +0530

    first time changed

--- octanet.py
+++ octanet.py
@@ -1,54 +1,39 @@
 import time
-
-print("Please insert your Card")
-
+print('Please insert your ATM Card')
 time.sleep(5)
-
-password=1234
-
-pin=int(input("enter your atm pin"))
-
-balance = 5000
-
-if pin==password:
+pin = int(input('Enter your 4 digit ATM Pin'))
+balance = 1000
+if pin == 1234:
     while True:
-        
-        print("""
-                1 == balance
-                2 == withdraw balance 
-                3 == deposit balance
-                4 == exit
-                """
-                )
-        
+        print("1 = Check Balance")
+        print("2 = Withdraw Money")
+        print("3 = Deposit Money")
+        print("4 = Exit")
         try:
-                option=int(input("Please enter your choise"))
+            option = int(input('Choose any option above'))
         except:
-            print("Please enter valid option")
-            
-            
-            if option == 1:
-                print(f"Your current balance is {balance}")
-                
-            if option == 2:
-                
-                withdraw_amount=int(input("please enter withdraw_amount"))
-                
-                balance=balance-withdraw_amount 
-                
-                print(f"Your updated balance is {balance}") 
-                
-            if option == 3:
-                
-                deposit_amount=int(input("please enter deposit amount"))
-                
-                balnce=balance+deposit_amount
-                print(f"{deposit_amount} is credited to your account")
-                print(f" your updated balance is{balance}" ) 
-                
-            if option == 4:
-                break
-                
-        else:
-            print("wrong pin please")             
-               
+            print("Please choose the valid option")
+        if option == 1:
+            print('----------------------------------')
+            print(f'Your current balance is {balance}')
+        if option == 2:
+            withdraw_money = int(input('Enter the Withdraw Amount'))
+            if withdraw_money < balance:
+                balance = balance - withdraw_money
+                print('----------------------------------')
+                print(f'{withdraw_money} is debited from your account')
+                print(f'Your current balance is {balance}')
+            else:
+                print('You do not have sufficient balance')
+        if option == 3:
+            deposit_money = int(input('Enter the deposit amount'))
+            balance = balance + deposit_money
+            print('----------------------------------')
+            print(f'{deposit_money} is credited to your account')
+            print(f'Your current balance is {balance}')
+        if option == 4:
+            print('Thank you for visiting our ATM Machine...See you Again')
+            break
+
+else:
+    print('You entered the wrong pin...Try Again')
